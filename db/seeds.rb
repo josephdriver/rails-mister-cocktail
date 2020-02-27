177794932d7e703d3f849e9d82d44da64b81231f
Joseph Driver <joseph.driver0@gmail.com>
2020-02-27 17:59:29 +0800

    functional cocktail site

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Martini")
+Cocktail.create(name: "Espresso Martini")
+Cocktail.create(name: "Gin and Tonic")
+Cocktail.create(name: "Red Rocket")
+Cocktail.create(name: "Scotch")
+Cocktail.create(name: "Rum and Coke")
